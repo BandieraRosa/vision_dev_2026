a453d171c8c50bf7a0bab44ac94a786cad5bc5c3
BandieraRossa <3132716198@qq.com>
2026-04-07 19:03:06 +0800

    fix: 修正相机像素格式。

--- src/hik_camera/src/hik_camera_node.cpp
+++ src/hik_camera/src/hik_camera_node.cpp
@@ -215,10 +215,9 @@ bool HikCameraNode::Read(cv::Mat& img, rclcpp::Time& timestamp)
 
   static const std::unordered_map<MvGvspPixelType, int> TYPE_MAP = {
       {PixelType_Gvsp_BayerGR8, cv::COLOR_BayerGR2RGB},
-      {PixelType_Gvsp_BayerRG8, cv::COLOR_BayerGR2RGB},
-      {PixelType_Gvsp_BayerGB8, cv::COLOR_BayerGR2RGB},
-      {PixelType_Gvsp_BayerBG8, cv::COLOR_BayerGR2RGB}};
-
+      {PixelType_Gvsp_BayerRG8, cv::COLOR_BayerRG2RGB},
+      {PixelType_Gvsp_BayerGB8, cv::COLOR_BayerGB2RGB},
+      {PixelType_Gvsp_BayerBG8, cv::COLOR_BayerBG2RGB}};
   auto it = TYPE_MAP.find(pixel_type);
   if (it == TYPE_MAP.end())
   {
